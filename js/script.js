2a297ae8a44a994aac8e337333275a878f336eb5
seyhomaru <seyhomaruuu13@gmail.com>
2024-01-23 10:38:12 -0300

    problem

--- js/script.js
+++ js/script.js
@@ -10,16 +10,3 @@ abrir.addEventListener("click",()=> {
 cerra.addEventListener("click",()=>{
     nav.classList.remove("visible");
 })
-
-
-var header =document.getElementById('header');
-
-window.addEventListener('scroll',()=> {
-    var scroll = window.scrollY
-
-    if (scroll>10) {
-        header.style.backgroundColor = '#121212'
-    }else {
-        header.style.backgroundColor = 'transparent'
-    }
-})
